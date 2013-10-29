f21f67ec123c22d6a862d3d39a85febe2dd6c783
dogura <daiki.ogura@soliton.co.jp>
2013-10-29 15:45:17 +0900

    refs #68 直近チャンネルをContextに追加

--- irc/Context.hx
+++ irc/Context.hx
@@ -8,10 +8,12 @@ import irc.Irc;
 class Context {
     public var irc(default, null): Irc;
     public var shared_deque(default, null): Deque<String>;
+    public var current_channel(default, default): String;
 
-    public function new(irc: Irc, shared_deque: Deque<String>) {
+    public function new(irc: Irc, shared_deque: Deque<String>, current_channel: String) {
         this.irc = irc;
         this.shared_deque = shared_deque;
+        this.current_channel = current_channel;
     }
 }
 
